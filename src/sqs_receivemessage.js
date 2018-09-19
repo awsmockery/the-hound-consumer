125a3fef042361ff9750384f2dff87622961a5c9
giseleml <giselemaraleo@gmail.com>
2018-09-19 19:22:55 -0300

    Correction JSON folder

--- src/sqs_receivemessage.js
+++ src/sqs_receivemessage.js
@@ -1,43 +1,35 @@
-const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-west-2'});
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'us-west-2' })
 
-const sqs = new AWS.SQS();
-const queueURL = "https://sqs.us-west-2.amazonaws.com/810028704317/awsmockery-queue";
+const sqs = new AWS.SQS()
+const queueURL = 'https://sqs.us-west-2.amazonaws.com/810028704317/awsmockery-queue'
 
 const params = {
- AttributeNames: [
-    "SentTimestamp"
- ],
- MaxNumberOfMessages: 10,
- MessageAttributeNames: [
-    "All"
- ],
- QueueUrl: queueURL,
- VisibilityTimeout: 5,
- WaitTimeSeconds: 0
-};
+  AttributeNames: [
+    'SentTimestamp'
+  ],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: [
+    'All'
+  ],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 5,
+  WaitTimeSeconds: 0
+}
 
-const receivePromise = new Promise (function(res, rej) {
-  sqs.receiveMessage(params, function(err, data) {
+const receive = () => {
+  sqs.receiveMessage(params, function (err, data) {
     if (err) {
-      console.log("Receive Error", err);
+      console.log('Receive Error', err)
     } else if (data.Messages) {
-      console.log(data.Messages);
+      console.log(data.Messages)
       var deleteParams = {
         QueueUrl: queueURL,
         ReceiptHandle: data.Messages[0].ReceiptHandle
-      };
-  }
-      /*
-      sqs.deleteMessage(deleteParams, function(err, data) {
-        if (err) {
-          console.log("Delete Error", err);
-        } else {
-          console.log("Message Deleted", data);
-        }
-      });
-    }*/
-    res()
-  });
-})
-Promise.all([receivePromise])
+      }
+    }
+  })
+}
+setInterval(() => {
+  receive()
+}, 1000)
